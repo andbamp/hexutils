a558f51ea79b0a7d3e5c5b8ab33b53f1351972bb
andbamp <andreasbampouris@gmail.com>
2021-04-05 16:02:20 +0000

    Code coverage on tests

--- tests/testthat/test-hexfind.R
+++ tests/testthat/test-hexfind.R
@@ -7,15 +7,20 @@ test_that("indexes of binary sequences are located correctly", {
   block_1 <- sapply(c("L", "o", "r", "e", "m"), charToRaw)
   exp_1 <- 1
   expect_equal(hexfind(file, block_1), exp_1)
+  expect_equal(hexfind(file, block_1, cpp = FALSE), exp_1)
   
   block_2 <- sapply(c("o", "r", "e", "m"), charToRaw)
   exp_2 <- c(2, 2661)
   expect_equal(hexfind(file, block_2), exp_2)
+  expect_equal(hexfind(file, block_2, cpp = FALSE), exp_2)
   
   block_3 <- charToRaw(".")
   test_3 <- length(hexfind(file, block_3))
   exp_3 <- 98
   expect_equal(test_3, exp_3)
+  
+  test_3b <- length(hexfind(file, block_3, cpp = FALSE))
+  expect_equal(test_3b, exp_3)
 })
 
 test_that("no match returns integer(0)", {
@@ -25,6 +30,7 @@ test_that("no match returns integer(0)", {
   block <- charToRaw("@")
   exp <- integer(0)
   expect_equal(hexfind(file, block), exp)
+  expect_equal(hexfind(file, block, cpp = FALSE), exp)
 })
 
 test_that("block smaller than x returns integer(0)", {
@@ -32,10 +38,12 @@ test_that("block smaller than x returns integer(0)", {
   block <- as.raw(rep(0, 10))
   exp <- integer(0)
   expect_equal(hexfind(block, x), exp)
+  expect_equal(hexfind(block, x, cpp = FALSE), exp)
 })
 
 test_that("indexes of block of repeated bytes is located in x of repeated bytes", {
   x <- as.raw(rep(0, 100))
   block <- as.raw(rep(0, 10))
   expect_equal(length(hexfind(x, block)), 91)
+  expect_equal(length(hexfind(x, block, cpp = FALSE)), 91)
 })
